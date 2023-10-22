556c845884cece47bb88d1d2603009eb6924cb5d
rgeli90 <rgeli90@gmail.com>
2023-10-21 19:02:19 -0500

    Added search functionality.

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -6,4 +6,11 @@ class LocationsController < ApplicationController
   def show
     @location = Location.find(params[:id])
   end
+
+  def search
+    @input = params[:query]
+    @selected_location = Location.find(params[:location])
+    @champions = Champion.where(location_id: @selected_location.id)
+    @champions_like = @champions.where("name LIKE ?", "#{@input}%")
+  end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,5 @@
 module ApplicationHelper
+  def location_dropdown
+    Location.order(:name).map { |location| [location.name, location.id] }
+  end
 end
