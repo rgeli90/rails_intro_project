d8a5f65c3092aba31981c69f3131ac01d5b174bb
rgeli90 <rgeli90@gmail.com>
2023-10-21 21:55:58 -0500

    Added pagination to champions page.

--- app/controllers/champions_controller.rb
+++ app/controllers/champions_controller.rb
@@ -1,9 +1,9 @@
 class ChampionsController < ApplicationController
-    def index
-        @champions = Champion.order(:name)
-    end
+  def index
+    @champions = Champion.order(:name).page(params[:page]).per(10)
+  end
 
-    def show
-        @champion = Champion.find(params[:id])
-    end
+  def show
+    @champion = Champion.find(params[:id])
+  end
 end
